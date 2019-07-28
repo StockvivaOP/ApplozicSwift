a8b668d9ca640d9f150c0a2fbada6cfc10f8aae8
OldPig <vissalking@gmail.com>
2019-07-28 23:00:33 +0800

    update config and remove useless delegate

--- Sources/Controllers/ALKBaseViewController.swift
+++ Sources/Controllers/ALKBaseViewController.swift
@@ -27,7 +27,6 @@ open class ALKBaseViewController: UIViewController, ALKConfigurable {
 
     override open func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        self.configuration.delegateConversationChatContentAction?.alkViewWillAppear(self)
         
         self.navigationController?.navigationBar.barTintColor = configuration.navigationBarBackgroundColor
         self.navigationController?.navigationBar.tintColor = configuration.navigationBarItemColor
@@ -45,13 +44,11 @@ open class ALKBaseViewController: UIViewController, ALKConfigurable {
     }
     open override func viewDidDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        self.configuration.delegateConversationChatContentAction?.alkViewDidDisappear(self)
     }
 
     override open func viewDidLoad() {
         super.viewDidLoad()
         checkPricingPackage()
-        self.configuration.delegateConversationChatContentAction?.alkViewDidLoad(self)
     }
 
     @objc func backTapped() {
--- Sources/Controllers/ALKConversationViewController.swift
+++ Sources/Controllers/ALKConversationViewController.swift
@@ -153,9 +153,6 @@ open class ALKConversationViewController: ALKBaseViewController, Localizable {
     
     //tag: stockviva start
     public var enableShowJoinGroupMode: Bool = false
-    public var groupRuleURL: String? = nil
-    public var groupURL: String? = nil
-    public var groupMetaDataURL: [String:Any]?
     private var discrimationViewHeightConstraint: NSLayoutConstraint?
     open var discrimationView: UIButton = {
         let view = UIButton()
