79cfbd7ae7d31ad7df3db144743f911a9738a65c
OldPigChu <oldpig.chu@moleculez.co>
2019-12-24 10:03:30 +0800

    clear show admin button status when send message

--- Sources/Controllers/ALKConversationViewController.swift
+++ Sources/Controllers/ALKConversationViewController.swift
@@ -1206,9 +1206,9 @@ open class ALKConversationViewController: ALKBaseViewController, Localizable {
             break;
         case .deleteMsg(let chatGroupHashID, let userHashID, let viewModel, let indexPath):
             print("DeleteMsg selected")
-             if let _chatGroupID = chatGroupHashID,
+             if /*let _chatGroupID = chatGroupHashID,*/
                 let _model = viewModel {
-                self.delegateConversationMessageBoxAction?.didMenuDeleteMsgClicked(chatGroupHashID:_chatGroupID, userHashID:userHashID, viewModel: _model, indexPath:indexPath)
+                //self.delegateConversationMessageBoxAction?.didMenuDeleteMsgClicked(chatGroupHashID:_chatGroupID, userHashID:userHashID, viewModel: _model, indexPath:indexPath)
                 self.viewModel.deleteMessagForAll(viewModel: _model, indexPath: indexPath, startProcess: {
                     self.delegateConversationChatContentAction?.isHiddenFullScreenLoading(false)
                 }) { (result, error) in
@@ -2437,6 +2437,7 @@ extension ALKConversationViewController {
     func sendMessageWithClearAllModel(completedBlock:@escaping ()->Void){
         //check model
         if self.viewModel.isUnreadMessageMode || self.viewModel.isDisplayMessageWithinUserListMode {
+            self.setShowAdminMessageButtonStatus(false)
             self.viewModel.messageSendUnderClearAllModel(startProcess: {
                 self.scrollingState = .idle
                 self.lastScrollingPoint = CGPoint.zero
